9f477416648fde291ee36e1ed31dd82c27a75b48
Matsuuu <huhta.matias@gmail.com>
2021-04-15 00:30:23 +0300

    Trigger sub events in parent keys

--- test/sub.test.js
+++ test/sub.test.js
@@ -17,6 +17,7 @@ it('Should react to sub events', async () => {
 
 it('Should react to sub events on subkeys', async () => {
     let foo = 0;
+
     sub("stoxy-state.bar.counter", (e) => {
         foo += 1
     });
@@ -68,4 +69,32 @@ it('Should return subkey value on sub events on subkeys', async () => {
 
     expect(foo).to.equal(1);
     expect(counterVal).to.equal(1);
+})
+
+it('Event in subkey should trigger sub on parent key', async () => {
+    let foo = 0;
+    let counterVal = 0;
+    sub("stoxy-foo", (e) => {
+        foo += 1
+        counterVal = e.data;
+    });
+
+    write("stoxy-foo", {
+        foo: "ff",
+        bar: {
+            counter: 0
+        }
+    });
+    clear("stoxy-foo");
+    write("stoxy-foo",
+        {
+            foo: "ff",
+            bar: {
+                counter: 0
+            }
+        }
+    );
+    await update("stoxy-foo.bar.counter", c => c += 1);
+
+    expect(foo).to.equal(4);
 })
