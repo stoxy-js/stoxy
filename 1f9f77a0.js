d31435c9eaba9f280b8c85c7289849c80f2c4212
Matsuuu <huhta.matias@gmail.com>
2022-03-14 20:30:41 +0000

    Deploying to gh-pages from @ stoxy-js/stoxy@71621f1b0bc5e0f88161bac1ac1e6fb83830a220 🚀

---
+++ 1f9f77a0.js
@@ -0,0 +1,48 @@
+import{L as t,e as r,h as o}from"./8e4cba22.js";let i,e,n=t=>t;customElements.define("inline-notification",class extends t{static get properties(){return{type:{type:String,reflect:!0},title:{type:String}}}constructor(){super(),this.title="",this.type="tip"}static get styles(){return r(i||(i=n`
+      :host {
+        padding: 0.1rem 1.5rem;
+        border-left-width: 0.5rem;
+        border-left-style: solid;
+        margin: 1rem 0;
+        display: block;
+      }
+
+      h3 {
+        font-weight: 600;
+        margin-bottom: 7px;
+        text-transform: uppercase;
+      }
+
+      :host([type='tip']) {
+        background-color: var(--inline-notification-tip-background-color, rgba(221, 221, 221, 0.3));
+        border-color: var(--inline-notification-tip-border-color, #42b983);
+      }
+
+      :host([type='warning']) {
+        background-color: var(
+          --inline-notification-warning-background-color,
+          rgba(255, 229, 100, 0.2)
+        );
+        border-color: var(--inline-notification-warning-border-color, #e7c000);
+      }
+
+      :host([type='danger']) {
+        background-color: var(--inline-notification-danger-background-color, rgba(192, 0, 0, 0.1));
+        border-color: var(--inline-notification-danger-border-color, #c00);
+      }
+
+      :host([type='warning']) h3 {
+        color: var(--inline-notification-warning-heading-color, #b29400);
+      }
+
+      :host([type='danger']) h3 {
+        color: var(--inline-notification-danger-heading-color, #900);
+      }
+
+      ::slotted(p) {
+        line-height: 1.7;
+      }
+    `))}render(){return o(e||(e=n`
+      <h3>${0}</h3>
+      <slot></slot>
+    `),this.title?this.title:this.type)}});
---
+++ 378e9adb.js
@@ -0,0 +1,1 @@
+import"./1f9f77a0.js";import"./8e4cba22.js";
