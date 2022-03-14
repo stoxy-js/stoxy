d8388c82906833b511c56e12cb976c9878b517f7
Matsuuu <huhta.matias@gmail.com>
2022-03-14 23:17:28 +0200

    Bear with me here

--- docs/rocket.config.mjs
+++ docs/rocket.config.mjs
@@ -51,7 +51,7 @@ const config = {
 
 config.presets = [rocketLaunch(), rocketBlog(), rocketSearch()];
 config.absoluteBaseUrl = "https://stoxy-js.github.io/stoxy/";
-config.pathPrefix = "/stoxy/";
-config.urlPath = "/stoxy/";
+//config.pathPrefix = "/stoxy/";
+//config.urlPath = "/stoxy/";
 
 export default config;
